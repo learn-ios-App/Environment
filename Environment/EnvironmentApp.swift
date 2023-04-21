61378d4a730a46c76cfa8d2f6a819caac79823e2
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-04-21 17:13:17 +0900

    Initial Commit

---
+++ Environment/EnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentApp.swift
+//  Environment
+//
+//  Created by 渡邊魁優 on 2023/04/21.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
